088ad597b9a5921047a465cecf4417ac92241599
tcpedersen <tobiascramerpedersen@gmail.com>
2023-03-04 13:38:12 +0100

    Add solutions for project 6.

--- projects/06/assembler.py
+++ projects/06/assembler.py
@@ -56,6 +56,33 @@ JUMP_MAP = {
 }
 
 
+SYMBOL_TABLE = {
+    'SP': '0',
+    'LCL': '1',
+    'ARG': '2',
+    'THIS': '3',
+    'THAT': '4',
+    'R0': '0',
+    'R1': '1',
+    'R2': '2',
+    'R3': '3',
+    'R4': '4',
+    'R5': '5',
+    'R6': '6',
+    'R7': '7',
+    'R8': '8',
+    'R9': '9',
+    'R10': '10',
+    'R11': '11',
+    'R12': '12',
+    'R13': '13',
+    'R14': '14',
+    'R15': '15',
+    'SCREEN': '16384',
+    'KBD': '24576',
+}
+
+
 def handle_ainstr(instr):
     return f'0{int(instr):015b}'
 
@@ -85,17 +112,17 @@ def assemble_single_line(asm_instruction):
     if is_a_instruction:
         return handle_ainstr(is_a_instruction.groups()[0])
 
-    is_c_instruction = re.match(r'([AMD]+)=([^;\\]+);([A-Z]{3})', asm_instruction)
+    is_c_instruction = re.match(r'([AMD]+)=([^;]+);([A-Z]{3})', asm_instruction)
     if is_c_instruction:
         dest, comp, jump = is_c_instruction.groups()
         return handle_cinstr(comp, dest, jump)
  
-    is_c_instruction = re.match(r'([AMD]+)=([^;\\]+)', asm_instruction)
+    is_c_instruction = re.match(r'([AMD]+)=([^;]+)', asm_instruction)
     if is_c_instruction:
         dest, comp = is_c_instruction.groups()
         return handle_cinstr(comp, dest, None)
 
-    is_c_instruction = re.match(r'([^;\\]+);([A-Z]{3})', asm_instruction)
+    is_c_instruction = re.match(r'([^;]+);([A-Z]{3})', asm_instruction)
     if is_c_instruction:
         comp, jump = is_c_instruction.groups()
         return handle_cinstr(comp, None, jump)
@@ -103,7 +130,56 @@ def assemble_single_line(asm_instruction):
     return None
 
 
+def build_symbol_table(asm_code):
+    new_asm_code = []
+    for line in asm_code:
+        line = re.sub(r'[\n\t\s]*', '', line)
+
+        # Ignore empty lines.
+        if line == '':
+            continue
+
+        # Ignore comments.
+        if line.startswith(r'//'):
+            continue
+
+        # Delete comments on lines.
+        if r'//' in line:
+            line = line.split(r'//')[0]
+
+        # Handle label definition.
+        is_label_definition = re.match(r'^\((.+)\)', line)
+        if is_label_definition:
+            label = is_label_definition.groups()[0]
+            SYMBOL_TABLE[label] = str(len(new_asm_code))
+            continue
+
+        new_asm_code.append(line)
+
+    return new_asm_code
+
+
+def handle_variables(asm_code):
+    RAM = 16
+    new_asm_code = []
+    
+    for line in asm_code:
+        is_a_instruction = re.match(r'^@([^/]+)', line)
+        if is_a_instruction and not re.match(r'^@[0-9]+', line):
+            variable = is_a_instruction.groups()[0]
+            if variable not in SYMBOL_TABLE:
+                SYMBOL_TABLE[variable] = str(RAM)
+                RAM += 1
+            line = line.replace(variable, SYMBOL_TABLE[variable])
+        new_asm_code.append(line)
+
+    return new_asm_code
+
+
 def assemble(asm_code):
+    asm_code = build_symbol_table(asm_code)
+    asm_code = handle_variables(asm_code)
+
     machine_code = []
     for line in asm_code:
         bin_code = assemble_single_line(line)
